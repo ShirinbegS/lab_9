85e10ac21ede8ced89f2495adc5ff46308a13e46
root <root@LAPTOP-SG2OVKS4>
2024-12-26 12:34:17 +0500

    laba

--- generation.py
+++ generation.py
@@ -4,6 +4,6 @@ def readfile(file, len):
             yield line[:len]
 
 file = 'gen.txt' 
-len = 4 #максимальная длина строки
-for line in readfile(file, len):
+len = 4 
+for line in readfile(file,len):
     print(line)
